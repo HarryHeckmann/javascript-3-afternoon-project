84637c3a55da7a535b062a0f9dc6b6c0b33f18d2
Chris <cabiediger@gmail.com>
2019-05-01 20:38:43 -0500

    problem 6 needs work

--- higher-order.js
+++ higher-order.js
@@ -21,9 +21,14 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(num) {
+  let evenArr = []
+  if(num % 2 === 0) {
+    return evenArr.push(num)
+  }
+}) 
 
-
+console.log(evenNumbers)
 
 ////////// PROBLEM 2 //////////
 
@@ -44,7 +49,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(price => price * 1.07)
 
 
 
@@ -63,7 +68,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((a,b) => a + b) //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
@@ -89,9 +94,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter((cp,i,arr) => arr[i]['CP'] > 200) 
 
-
+console.log(myStrongest)
 
 ////////// PROBLEM 5 //////////
 
@@ -106,9 +111,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.map(order => order['price'] * (1 + order['tax']))
+ // Code here
 
-
+console.log(orderTotals)
 
 ////////// PROBLEM 6 //////////
 
@@ -126,6 +132,7 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.reduce((a,b,i,arr) => arr[a] + arr[b]) // NEEDS WORK
 
 
+console.log(bobsTotal)
